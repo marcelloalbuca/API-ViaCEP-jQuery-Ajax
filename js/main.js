b523eca3a70f09f3748090cf455dca8552cfd727
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-04-27 21:11:19 -0300

    testando funcao novo layout

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@ function consultaCep(){
 
     var cep = document.getElementById("cep").value;
     var url = "https://viacep.com.br/ws/" + cep + "/json/";
-    console.log(cep);
+    //console.log(cep);
 
     $.ajax({
         url: url,
         type: "GET",
         success: function(response){
-           // console.table(response);
+            console.table(response);
            // console.log(response.bairro);
 
-           $("#logradouro").html(response.logradouro);
-           document.getElementById("bairro").innerHTML = response.bairro;
-           document.getElementById("localidade").innerHTML = response.localidade;
-           document.getElementById("uf").innerHTML = response.uf;
+           //$("#logradouro").html(response.logradouro);
+           //document.getElementById("bairro").innerHTML = response.bairro;
+           //document.getElementById("localidade").innerHTML = response.localidade;
+           //document.getElementById("uf").innerHTML = response.uf;
 
         }
     })
